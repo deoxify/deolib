ae50b852a31ce55baf958113ab5a47dfb4eee7e0
deo <53539012+deoxify@users.noreply.github.com>
2024-12-05 01:26:42 +0300

    Update deolib.js

--- deolib.js
+++ deolib.js
@@ -18,9 +18,10 @@ class Rectangle {
 
 let _ctx;
 let _firstTime = performance.now();
-let _lastTime = performance.now();
-let _deltaTime = 0;
+let _lastTime;
+let _frameTime = 0;
 let _fps = 0;
+let _avgFPS = 0;
 let _fpsUpdateInterval = 250;
 let _fpsFontSize;
 let _cachedFont;
@@ -67,7 +68,7 @@ const _mouse = {
   prev: {
     [MOUSE_LEFT_BUTTON]: false,
     [MOUSE_MIDDLE_BUTTON]: false,
-    [MOUSE_RIGHT_BUTTON]: false,
+    [MOUSE_RIGHT_BUTTON]: false
   }
 };
 
@@ -86,34 +87,34 @@ const IsKeyDown = keyCode => keyCode in _keyboard && _keyboard[keyCode];
 const IsKeyUp = keyCode => keyCode in _keyboard && !_keyboard[keyCode];
 const IsKeyPressed = keyCode => keyCode in _keyboard && !_keyboard.prev[keyCode] && _keyboard[keyCode];
 const IsKeyReleased = keyCode => keyCode in _keyboard && _keyboard.prev[keyCode] && !_keyboard[keyCode];
-const GetMousePosition = () => ({x: _mouse.x, y: _mouse.y});
+const GetMousePosition = () => ({ x: _mouse.x, y: _mouse.y });
 const GetMouseX = () => _mouse.x;
 const GetMouseY = () => _mouse.y;
 const GetScreenWidth = () => _ctx.canvas.width;
 const GetScreenHeight = () => _ctx.canvas.height;
 const GetScreenRect = () => _canvasRect;
-const GetFrameTime = () => _deltaTime;
-const GetFPS = () => Math.floor(_fps);
+const GetFrameTime = () => _frameTime;
+const GetFPS = () => _fps;
 const GetTime = () => (performance.now() - _firstTime) / 1000;
 
 function InitWindow(width, height, title) {
   const _canvas = document.createElement("canvas");
-  Object.assign(_canvas, { width, height });
   _ctx = _canvas.getContext("2d");
-  _fpsFontSize = Math.floor((_ctx.canvas.width + _ctx.canvas.height) / 64);
+  _canvas.width = width;
+  _canvas.height = height;
   _canvas.oncontextmenu = e => e.preventDefault();
-  _canvas.onmouseenter = () => _mouseInCanvas = true;
-  _canvas.onmouseleave = () => _mouseInCanvas = false;
+  _canvas.onmouseenter = () => (_mouseInCanvas = true);
+  _canvas.onmouseleave = () => (_mouseInCanvas = false);
   _canvas.onmousemove = e => {
     const rect = _canvas.getBoundingClientRect();
     _mouse.x = Math.floor(e.clientX - rect.left);
     _mouse.y = Math.floor(e.clientY - rect.top);
   };
   _canvas.onmousedown = e => {
-      if (_mouseInCanvas) _mouse[e.button] = true;
+    if (_mouseInCanvas) _mouse[e.button] = true;
   };
   _canvas.onmouseup = e => {
-      if (_mouseInCanvas) _mouse[e.button] = false;
+    if (_mouseInCanvas) _mouse[e.button] = false;
   };
   window.onkeydown = e => {
     if (e.code.length) _keyboard[e.code] = true;
@@ -139,15 +140,18 @@ function InitWindow(width, height, title) {
     overflow: "hidden"
   });
 
-  _ctx.textAlign = "left";
-  _ctx.textBaseline = "top";
-
-  document.body.appendChild(_canvas);
-  document.title = title;
-
   window.onload = () => {
-      _canvasRect = new Rectangle(0, 0, _ctx.canvas.width, _ctx.canvas.height);
-      SetFont("bold 20px monospace, system");
+    _ctx.textAlign = "left";
+    _ctx.textBaseline = "top";
+    _ctx.lineCap = "square";
+    document.body.appendChild(_canvas);
+    document.title = title;
+    _canvasRect = new Rectangle(0, 0, _ctx.canvas.width, _ctx.canvas.height);
+    _fpsFontSize = Math.floor((_ctx.canvas.width + _ctx.canvas.height) / 64);
+    _ctx.textRendering = "geometricPrecision";
+    _ctx.imageSmoothingEnabled = true;
+    SetLineThick(2);
+    SetFont("20px monospace, system");
     if (typeof main === "function") {
       _startGameLoop(main);
     } else {
@@ -185,13 +189,12 @@ function _startGameLoop(callback) {
     }
   }
 
-  function loop() {
-    const now = performance.now();
-    _deltaTime = (now - _lastTime) / 1000;
-    _lastTime = now;
+  function loop(currentTime = 0) {
+    _frameTime = (currentTime - _lastTime) / 1000;
+    _lastTime = currentTime;
 
     if (isPageVisible) {
-      _fps = 1 / _deltaTime;
+      _updateFPS();
       callback();
       _updatePrevMouseState();
       _updatePrevKeyboardState();
@@ -223,17 +226,26 @@ function SetLineThick(thickness) {
   _ctx.lineWidth = thickness;
 }
 
+function Clamp(value, min, max) {
+  return Math.max(min, Math.min(max, value));
+}
+
+function _updateFPS() {
+  _avgFPS = 0.05 / _frameTime + 0.95 * _avgFPS;
+  _fps = Math.floor(_avgFPS);
+}
+
 const DrawFPS = (() => {
   let FPSTimer = 0;
   let lastFPS = 0;
 
   return function (x, y, updateInterval = _fpsUpdateInterval) {
-    FPSTimer += _deltaTime * 1000;
+    FPSTimer += _frameTime * 1000;
     if (FPSTimer >= updateInterval) {
       lastFPS = GetFPS();
       FPSTimer = 0;
     }
-    DrawText(`${lastFPS} FPS`, x, y, _fpsFontSize, DARKGREEN, {weight: 900});
+    DrawText(`${lastFPS} FPS`, x, y, _fpsFontSize, DARKGREEN, { weight: 900 });
   };
 })();
 
@@ -242,24 +254,67 @@ function DrawRectangle(x, y, width, height, color) {
   _ctx.fillRect(x, y, width, height);
 }
 
-function DrawRectangleRec(rect, color) {
-  DrawRectangle(rect.x, rect.y, rect.width, rect.height, color);
-}
-
 function DrawRectangleLines(x, y, width, height, color) {
   _ctx.strokeStyle = color;
   _ctx.strokeRect(x, y, width, height);
 }
 
+function DrawRectangleRec(rect, color) {
+  DrawRectangle(rect.x, rect.y, rect.width, rect.height, color);
+}
+
+function DrawRectangleLinesRec(rect, color) {
+  DrawRectangleLines(rect.x, rect.y, rect.width, rect.height, color);
+}
+
 function DrawRectangleLinesEx(rect, thickness, color) {
+  const x = rect.x + thickness * 0.5;
+  const y = rect.y + thickness * 0.5;
+  _ctx.save();
   _ctx.strokeStyle = color;
+  _ctx.lineWidth = thickness;
+  _ctx.strokeRect(x, y, rect.width - thickness, rect.height - thickness);
+  _ctx.restore();
+}
+
+function DrawRectangleRounded(rect, roundness, color) {
+  _ctx.beginPath();
+  _ctx.fillStyle = color;
+  _ctx.roundRect(rect.x, rect.y, rect.width, rect.height, roundness);
+  _ctx.fill();
+}
+
+function DrawRectangleRoundedLines(rect, roundness, color) {
+  _ctx.beginPath();
+  _ctx.strokeStyle = color;
+  _ctx.roundRect(rect.x, rect.y, rect.width, rect.height, roundness);
+  _ctx.stroke();
+}
+
+function DrawRectangleRoundedLinesEx(rect, roundness, thickness, color) {
   _ctx.save();
   _ctx.lineWidth = thickness;
-  const x = rect.x + thickness / 2;
-  const y = rect.y + thickness / 2;
-  const w = rect.width - thickness;
-  const h = rect.height - thickness;
-  _ctx.strokeRect(x, y, w, h);
+  DrawRectangleRoundedLines(rect, roundness, color);
+  _ctx.restore();
+}
+
+function DrawSquareGrid(x, y, cols, rows, cellSize, color = LIGHTGRAY, thickness = 2) {
+  _ctx.save();
+  _ctx.strokeStyle = color;
+  _ctx.lineWidth = thickness;
+
+  _ctx.beginPath();
+  for (let i = 0; i <= cols; i++) {
+    const xPos = x + i * cellSize;
+    _ctx.moveTo(xPos, y);
+    _ctx.lineTo(xPos, y + rows * cellSize);
+  }
+  for (let j = 0; j <= rows; j++) {
+    const yPos = y + j * cellSize;
+    _ctx.moveTo(x, yPos);
+    _ctx.lineTo(x + cols * cellSize, yPos);
+  }
+  _ctx.stroke();
   _ctx.restore();
 }
 
@@ -268,19 +323,18 @@ function CheckCollisionPointRec(point, rect) {
 }
 
 function DrawLine(x1, y1, x2, y2, color, thickness) {
-  function stroke() {
-    _ctx.stroke();
-  }
   _ctx.strokeStyle = color;
   _ctx.beginPath();
   _ctx.moveTo(x1, y1);
   _ctx.lineTo(x2, y2);
-  if (thickness) {
+  if (thickness && _ctx.lineWidth !== thickness) {
     _ctx.save();
     _ctx.lineWidth = thickness;
-    stroke();
+  }
+  _ctx.stroke();
+  if (thickness && _ctx.lineWidth !== thickness) {
     _ctx.restore();
-  } else stroke();
+  }
   _ctx.closePath();
 }
 
@@ -298,6 +352,14 @@ function DrawCircleLines(centerX, centerY, radius, color) {
   _ctx.stroke();
 }
 
+function DrawCircleV(center, radius, color) {
+  DrawCircle(center.x, center.y, radius, color);
+}
+
+function DrawCircleLinesV(center, radius, color) {
+  DrawCircleLines(center.x, center.y, radius, color);
+}
+
 function DrawCircleLinesEx(circle, thickness, color) {
   _ctx.save();
   SetLineThick(thickness);
@@ -305,119 +367,55 @@ function DrawCircleLinesEx(circle, thickness, color) {
   _ctx.restore();
 }
 
-function SetFont(fontStr) {
-  _ctx.font = _cachedFont= fontStr.trim();
-  _ctx.fillText("cache", -9999, -9999);
+function DrawPolyLines(center, sides, radius, rotation, color) {
+  if (sides < 3) return;
+  _ctx.beginPath();
+  _ctx.moveTo(center.x + radius * Math.cos(rotation), center.y + radius * Math.sin(rotation));
+  for (let i = 1; i < sides; i++) {
+    const angle = rotation + (i * 2 * Math.PI) / sides;
+    _ctx.lineTo(center.x + radius * Math.cos(angle), center.y + radius * Math.sin(angle));
+  }
+  _ctx.closePath();
+  _ctx.strokeStyle = color;
+  _ctx.stroke();
+}
+
+function DrawPoly(center, sides, radius, rotation, color) {
+  if (sides < 3) return;
+  DrawPolyLines(center, sides, radius, rotation);
+  _ctx.fillStyle = color;
+  _ctx.fill();
+}
+
+function DrawTriangle(v1, v2, v3, color) {
+  _ctx.beginPath();
+  _ctx.moveTo(v1.x, v1.y);
+  _ctx.lineTo(v2.x, v2.y);
+  _ctx.lineTo(v3.x, v3.y);
+  _ctx.closePath();
+  _ctx.fillStyle = color;
+  _ctx.fill();
+}
+
+function SetFont(newFont) {
+  if (_cachedFont != newFont) {
+    _ctx.font = newFont;
+    _ctx.fillText("caching", -9999, -9999);
+  } else _ctx.font = _cachedFont;
 }
 
 function DrawText(text, x, y, size = null, color = null, { align, baseline, weight, style, font } = {}) {
   if (!String(text).length) return;
   _ctx.save();
-  if (color && _ctx.fillStyle !== color) _ctx.fillStyle = color;
-  if (align && _ctx.textAlign !== align) _ctx.textAlign = align;
-  if (baseline && _ctx.textBaseline !== baseline) _ctx.textBaseline = baseline;
+  if (color && _ctx.fillStyle != color) _ctx.fillStyle = color;
+  if (align && _ctx.textAlign != align) _ctx.textAlign = align;
+  if (baseline && _ctx.textBaseline != baseline) _ctx.textBaseline = baseline;
   if (size || weight || style || font) {
     const fontFamily = font || _ctx.font.split("px")[1];
     const newFont = `${style || ""} ${weight || ""} ${size || parseInt(_ctx.font)}px ${fontFamily}`.trim();
-    if (_cachedFont !== newFont) {
-      _ctx.font = newFont;
-      _cachedFont = newFont;
-    }
+    // const sanitizedNewFont = newFont.split(" ").filter(t => t).map(t => t.trim()).join(" ");
+    SetFont(newFont);
   }
   _ctx.fillText(text, x, y);
   _ctx.restore();
-}
-
-class _TestObject {
-  static _objects = [];
-
-  constructor() {
-    this.color = GetRandomColor();
-    const size = GetRandomValue(_ctx.canvas.width / 64, _ctx.canvas.width / 16);
-    const x = GetRandomValue(size, _ctx.canvas.width - size);
-    const y = GetRandomValue(size, _ctx.canvas.height - size);
-
-    this.shape = Math.random() < 0.5 ? new Circle(new Vector2(x, y), size) : new Rectangle(x, y, size, size);
-    this.hallow = Math.random() < 0.5;
-    if (this.hallow) this.thickness = GetRandomValue(size / 8, size / 4);
-
-    const getSpeed = (min, max) => GetRandomValue(min, max) * (Math.random() < 0.5 ? -1 : 1);
-    this.vx = getSpeed(_ctx.canvas.width / 8, _ctx.canvas.width / 2);
-    this.vy = getSpeed(_ctx.canvas.height / 8, _ctx.canvas.height / 2);
-  }
-
-  _render() {
-    const canvasRect = new Rectangle(0, 0, _ctx.canvas.width, _ctx.canvas.height);
-    const dx = this.vx * _deltaTime,
-      dy = this.vy * _deltaTime;
-
-    if (this.shape instanceof Circle) {
-      this.hallow
-        ? DrawCircleLinesEx(this.shape, this.thickness, this.color)
-        : DrawCircle(this.shape.center.x, this.shape.center.y, this.shape.radius, this.color);
-
-      this._handleCircleCollision(canvasRect, dx, dy);
-    } else if (this.shape instanceof Rectangle) {
-      this.hallow
-        ? DrawRectangleLinesEx(this.shape, this.thickness, this.color)
-        : DrawRectangleRec(this.shape, this.color);
-
-      this._handleRectangleCollision(canvasRect, dx, dy);
-    }
-  }
-
-  _handleCircleCollision(rect, dx, dy) {
-    const c = this.shape.center,
-      r = this.shape.radius;
-
-    if (c.x - r + dx < 0) {
-      c.x = r;
-      this.vx = -this.vx;
-    } else if (c.x + r + dx > rect.width) {
-      c.x = rect.width - r;
-      this.vx = -this.vx;
-    }
-
-    if (c.y - r + dy < 0) {
-      c.y = r;
-      this.vy = -this.vy;
-    } else if (c.y + r + dy > rect.height) {
-      c.y = rect.height - r;
-      this.vy = -this.vy;
-    }
-
-    c.x += dx;
-    c.y += dy;
-  }
-
-  _handleRectangleCollision(rect, dx, dy) {
-    const s = this.shape;
-
-    if (s.x + dx < 0) {
-      s.x = 0;
-      this.vx = -this.vx;
-    } else if (s.x + s.width + dx > rect.width) {
-      s.x = rect.width - s.width;
-      this.vx = -this.vx;
-    }
-
-    if (s.y + dy < 0) {
-      s.y = 0;
-      this.vy = -this.vy;
-    } else if (s.y + s.height + dy > rect.height) {
-      s.y = rect.height - s.height;
-      this.vy = -this.vy;
-    }
-
-    s.x += dx;
-    s.y += dy;
-  }
-}
-
-function AddTestObjects(count = 1) {
-  for (let i = 0; i < count; ++i) _TestObject._objects.push(new _TestObject());
-}
-
-function RenderTestObjects() {
-  for (const obj of _TestObject._objects) obj._render();
 }
