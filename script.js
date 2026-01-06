711160299c45b2db1e916a6a86e91e0dc28ba017
deo <53539012+deoxify@users.noreply.github.com>
2026-01-07 00:05:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const config = dl.initCanvas({
-    width: 800,
-    height: 450,
+    width: 640,
+    height: 480,
     title: "Example",
 });
 
 const ball = {
-    center: dl.Vector2(config.width >> 1, config.height >> 1),
+    center: dl.Vector2(config.width * 0.5, config.height * 0.5),
     radius: 48,
     vel: dl.Vector2(1.5, 1.5)
 };
@@ -19,11 +19,11 @@ dl.main = () => {
     ball.center.x += Math.sin(angle) * ball.vel.x;
     ball.center.y += Math.cos(angle) * ball.vel.y;
 
-    const edgeCollision = dl.getCollisionCircleBounds(ball, dl.getCanvasRect());
-    if (edgeCollision.left || edgeCollision.right) ball.vel.x = -ball.vel.x;
-    if (edgeCollision.top || edgeCollision.bottom) ball.vel.y = -ball.vel.y;
+    const edgeCollision = dl.getCollisionCircleBounds(ball.center, ball.radius, dl.getCanvasRect());
+    if (edgeCollision.left || edgeCollision.right) ball.vel.x *= -1;
+    if (edgeCollision.top || edgeCollision.bottom) ball.vel.y *= -1;
 
-    if (dl.checkCollisionPointCircle(dl.getMousePosition(), ball)) {
+    if (dl.checkCollisionPointCircle(dl.getMousePosition(), ball.center, ball.radius)) {
         timer = Math.min(timer + dl.getFrameTime(), 99.99);
         const fontSize = Math.min(timer * 30, config.width * 0.2);
         dl.drawTextCentered(`${timer.toFixed(2)}s`, config.width * 0.5, config.height * 0.5, fontSize, dl.DARKGRAY);
@@ -33,5 +33,6 @@ dl.main = () => {
         timer = 0;
         dl.drawCircleLinesV(ball.center, ball.radius, dl.RED);
     }
+
     dl.drawText(`Best: ${bestTime.toFixed(2)}s`, 15, config.height - 30, 20, dl.GREEN);
 }
